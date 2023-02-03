11ff157f71dda1edf18d2807870e75c281ee187d
Victor Owiti <viowiti12@gmail.com>
2023-02-03 13:52:08 +0300

    Added the navbar with logo

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import { AppBar, Container, Stack, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import Logo from "../assets/logo.png";
+const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Container>
+        <Stack direction="row" alignItems="center" gap="1rem">
+          <img
+            src={Logo}
+            alt="logo"
+            style={{ width: "50px", height: "100%" }}
+          />
+          <Typography
+            component="h2"
+            sx={{
+              fontSize: "1.5rem",
+            }}
+          >
+            Workforce Jobs
+          </Typography>
+        </Stack>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default NavBar;
